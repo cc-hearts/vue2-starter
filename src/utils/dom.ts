d530df50a9700203ce29b2231e76b1f64563794c
Carl Chen <ccheartszzz@gmail.com>
2024-09-27 23:38:57 +0800

    perf: optimize useOverflowHidden fn

---
+++ src/utils/dom.ts
@@ -0,0 +1,13 @@
+
+
+export function getPadding(el: HTMLElement) {
+  const parseInt = (target: string) => Number.parseInt(target) || 0
+
+  const style = window.getComputedStyle(el, null)
+  const left = parseInt(style.paddingLeft)
+  const right = parseInt(style.paddingRight)
+  const top = parseInt(style.paddingTop)
+  const bottom = parseInt(style.paddingBottom)
+
+  return { left, right, top, bottom }
+}
